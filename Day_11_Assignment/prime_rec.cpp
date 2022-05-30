f83a99663fbd20fbdaccea5e4c475e4773beebba
Praveen Savadatti <praveen08.ps@gmail.com>
2022-05-30 17:05:17 +0530

    Armstrong push

--- Day_11_Assignment/prime_rec.cpp
+++ Day_11_Assignment/prime_rec.cpp
@@ -5,6 +5,13 @@ int prime(int);
 
 int main()
 {
+    int n, ans;
+
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    ans = prime(n);
+    cout<<endl<<n<<" is a prime number";
 
 }
 
--- Day_7_Assignment/Armstrong.cpp
+++ Day_7_Assignment/Armstrong.cpp
@@ -1,8 +1,44 @@
 #include<iostream>
 #include<iomanip>
+#include<cmath>
 using namespace std;
 
 int main()
 {
-    
+    int n, i, power, temp, sum, rem;
+
+    cout<<"Enter the end value of the range: ";
+    cin>>n;
+
+    for(i=1; i<n; i++)
+    {
+        power = 0;
+
+        temp = i;
+
+        while(temp>0)
+        {
+            power++;
+            temp /= 10;
+        }
+        //cout<<power;
+
+        sum = 0;
+
+        temp = i;
+
+        while(temp > 0)
+        {
+            rem = temp % 10;
+            sum = sum + pow(rem, power);
+            temp /= 10;
+        }
+
+        //cout<<sum<<" "<<i<<endl;
+
+        if(sum == i)
+            cout<<i<<" ";
+
+
+    }   
 }
